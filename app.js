ddda2c1266bee05d0f9a971d88f1aaef3244eb29
ohad partuck <ohadpartuck@gmail.com>
2014-04-21 14:30:05 +0300

    inintial commit

--- app.js
+++ app.js
@@ -31,29 +31,15 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    var error = (app.get('env') === 'development') ? err : {} ;
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: error
     });
 });
 
 
 module.exports = app;
+
