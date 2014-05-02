716cacc9769d9338617215d5a22490625df3bebf
ohad partuck <ohadpartuck@gmail.com>
2014-05-02 16:34:50 +0300

    working on sanger new products

--- init/app_setup.js
+++ init/app_setup.js
@@ -19,10 +19,14 @@ function require_settings(namespace){
 }
 
 function defineGlobalFunctions(){
+    //TODO - all these function - extract to a helper module
     isProduction                = function() { return ENV == 'production' };
     useStub                     = function(use_stub_setting) { return use_stub_setting && !isProduction() };
     GenericOnGetError           = function(params){
         //TODO - catch all errors not here but by emitting an event
-        console.log('GenericOnGetError got this ' + JSON.stringify(params))
-    }
+        console.log('GenericOnGetError got this ' + JSON.stringify(params));
+    };
+    genericNewObjectCallback    = function(params){
+        console.log('genericNewObjectCallback got this ' + JSON.stringify(params));
+    };
 }
