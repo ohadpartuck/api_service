7cdc3b37ad872de81f12c2b242576231cb02c8ab
ohad partuck <ohadpartuck@gmail.com>
2014-05-07 09:22:22 +0300

    adding routes to users api

--- routes/sanger/users_api.js
+++ routes/sanger/users_api.js
@@ -1,6 +1,30 @@
 module.exports = function (router, namespace) {
-    router.get(namespace + '/ab', function(req, res) {
-        res.json({'users_main': 123});
+    router.post(namespace + '/login', function(req, res) {
+        res.json({'login': true});
+    });
+
+    router.get(namespace + '/logout', function(req, res) {
+        res.json({'logout': true});
+    });
+
+    router.post(namespace + '/signup', function(req, res) {
+        res.json({'signup': true});
+    });
+
+    router.get(namespace + '/forgot', function(req, res) {
+        res.json({'forgotGet': true});
+    });
+
+    router.post(namespace + '/forgot', function(req, res) {
+        res.json({'forgotPost': true});
+    });
+
+    router.get(namespace + '/reset/:token', function(req, res) {
+        res.json({'reset_token_get': true});
+    });
+
+    router.post(namespace + '/reset/:token', function(req, res) {
+        res.json({'reset_token_post': true});
     });
 
     return router;
